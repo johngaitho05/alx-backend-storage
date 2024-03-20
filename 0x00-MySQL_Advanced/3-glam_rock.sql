08326433aeafef4ca62cb9579ddecc8716d0d18e
John Gaitho <johngaitho05@gmail.com>
2024-03-20 21:55:36 +0300

    order desc

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Task 3
 
 -- lists all band with 'Glam rock' as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
