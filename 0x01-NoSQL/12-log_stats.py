324cd7838578eaa796f5d6f4897d1bbe565ec536
John Gaitho <johngaitho05@gmail.com>
2024-03-26 17:11:12 +0300

    fix requirement issue

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,15 +3,16 @@
 about Nginx logs stored in MongoDB:"""
 from pymongo import MongoClient
 
-local_client = MongoClient()
-collection = local_client['logs']['nginx']
+if __name__ == "__main__":
+    local_client = MongoClient()
+    collection = local_client['logs']['nginx']
 
-count = collection.count_documents({})
-print('{} logs'.format(count))
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-print('Methods:')
-for mtd in methods:
-    print('    method {}: {}'.format(
-        mtd, collection.count_documents({'method': mtd})))
-print('{} status check'.format(
-    collection.count_documents({'method': 'GET', 'path': '/status'})))
+    count = collection.count_documents({})
+    print('{} logs'.format(count))
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
+    for mtd in methods:
+        print('    method {}: {}'.format(
+            mtd, collection.count_documents({'method': mtd})))
+    print('{} status check'.format(
+        collection.count_documents({'method': 'GET', 'path': '/status'})))
