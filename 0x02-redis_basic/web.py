a53cf830c3091cbef942d15ac05cbf5f23053f1e
John Gaitho <johngaitho05@gmail.com>
2024-03-27 20:06:05 +0300

    request caching

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
-""" A simple caching for getting web page content """
-import functools
-from typing import Callable
+"""
+This module contains get_page function
+"""
 
 import redis
+from functools import wraps
+from typing import Callable
+
 import requests
-
-store = redis.Redis()
-
-
-def request_memo(func: Callable) -> Callable:
-    """Implements caching on a request"""
-
-    @functools.wraps(func)
-    def invoker(url):
-        # Initialize Redis client
-        redis_client = redis.Redis()
-
-        # Track the number of accesses to the URL
-        count_key = f"count:{url}"
-        redis_client.incr(count_key)
-
-        # Retrieve cached HTML content if available
-        cached_html = redis_client.get(url)
-        if cached_html:
-            return cached_html.decode('utf-8')
-
-        # Fetch HTML content using requests
-        html_content = func(url)
-
-        # Cache the HTML content with expiration time of 10 seconds
-        redis_client.setex(url, 10, html_content)
-
-        return html_content
-
-    return invoker
+from requests import get
 
 
-@request_memo
+r = redis.Redis()
+
+
+def cache_http_request(fxn: Callable) -> Callable:
+    """
+    Decorator that caches a http request in Redis
+    """
+    @wraps(fxn)
+    def wrapper(url: str) -> str:
+        if type(url) != str:
+            return ""
+
+        r.incr(f"count:{url}")
+        result = r.get(f"{url}")
+        if result and type(result) == bytes:
+            return result.decode("utf-8")
+
+        result = fxn(url)
+        r.setex("{}".format(url), 10, result)
+        return result
+
+    return wrapper
+
+
+@cache_http_request
 def get_page(url: str) -> str:
-    """Retrieves the content of a page"""
-    response = requests.get(url)
-    return response.text
+    """
+    obtain the HTML content of a particular URL and returns it
+    """
+    if type(url) != str:
+        return ""
+
+    return requests.get(url).text
